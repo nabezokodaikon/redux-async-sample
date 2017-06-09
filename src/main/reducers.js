492bb9cff6219aa73f23299ee6f86d130a3f381f
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-06-10 06:58:49 +0900

    Add reducers

---
+++ src/main/reducers.js
@@ -0,0 +1,49 @@
+import { combineReducers } from "redux";
+import {
+  INPUT_USER,
+  REQUEST_REPOS,
+  RECEIVE_REPOS,
+  INVALIDATE_RESULT
+} from "./actionTypes";
+
+function inputedUser(state = "", action) {
+  switch (action.type) {
+    case: INPUT_USER:
+      return action.user;
+    default:
+      return state;
+  }
+}
+
+function getReposByUser(state = {
+  isFetching: false,
+  didInvalidate: false,
+  errorMessage: "",
+  repos: []
+}, action) {
+  switch (action.type) {
+    case INVALIDATE_RESULT:
+      return Object.assign({}, state, {
+        didInvalidate: true
+      });
+    case REQUEST_REPOS:
+      return Object.assign({}, state, {
+        isFetching: true,
+        didInvalidate: true
+      });
+    case RECEIVE_REPOS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: false,
+        errorMessage: action.errorMessage,
+        repos: action.repos
+      });
+    default:
+      return state;
+  }
+}
+
+export default rootReducer = combineReducers({
+  inputedUser,
+  getReposByUser
+});
