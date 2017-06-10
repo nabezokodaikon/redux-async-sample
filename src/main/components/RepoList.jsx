2a16b26b08d9a39f8a767a12b8ba0ae264901b8a
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-06-10 15:29:44 +0900

    Add RepoList component

---
+++ src/main/components/RepoList.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class RepoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul>
+        {
+          this.props.repos.map((repo, i) => {
+            return (
+              <li key={i}>
+                <a href={repo.url}>{repo.name}</a>
+              </li>
+            )<li>
+          });
+        }
+      </ul>
+    );
+  }
+}
+
+RepoList.propTypes = {
+  repos: PropTypes.arrayOf(
+    PropTypes.shape({
+      url: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired
+    })  
+  ).isRequired
+};
